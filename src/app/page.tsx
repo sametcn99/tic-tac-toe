1626fbfc1c1aba08fc83de3b6b8182b2f292e58a
sametcn99 <sametcn99@gmail.com>
2023-11-12 03:01:07 +0300

    added score tracking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-"use client";
-import { useState, useEffect, useCallback } from "react";
-import { calculateWinner, getBestMove } from "@/app/utils/utils";
+"use client"; // This seems to be an import or configuration related to a specific tool or environment
+
+import { useState, useEffect, useCallback } from "react"; // Importing necessary hooks from React
+import { calculateWinner, getBestMove } from "@/app/utils/utils"; // Importing utility functions
+import { FiRefreshCw } from "react-icons/fi"; // Importing a refresh icon
+
 export default function Home() {
   const initialBoard = Array(9).fill(null); // Create an initial game board with 9 empty squares
   const [board, setBoard] = useState(initialBoard); // Create a state variable to hold the game board
   const [xIsNext, setXIsNext] = useState(true); // Create a state variable to track whose turn it is (X or O)
   const [firstMove, setFirstMove] = useState(false); // Create a state variable to track if it's the first move
+  const [score, setScore] = useState({ X: 0, O: 0 }); // Create a state variable to track the score
 
   const winner = calculateWinner(board); // Determine if there's a winner
   const isDraw = !winner && board.every((square) => square); // Check if it's a draw
@@ -28,7 +32,15 @@ export default function Home() {
     [board, xIsNext]
   );
 
+  const handleResetScore = () => {
+    setScore({ X: 0, O: 0 }); // Reset the score
+  };
+
   useEffect(() => {
+    setScore((prevScore) => ({
+      ...prevScore,
+      [winner as "X" | "O"]: prevScore[winner as "X" | "O"] + 1,
+    })); // Update the score based on the winner
     if (xIsNext && firstMove === false) {
       // If it's X's turn and it's the first move
       const randomIndex = Math.floor(Math.random() * 9); // Generate a random index
@@ -39,7 +51,7 @@ export default function Home() {
       const bestMove = getBestMove(board, "O", xIsNext); // Calculate the best move for X
       handleClick(bestMove); // Perform the best move
     }
-  }, [xIsNext, firstMove, handleClick, board]);
+  }, [xIsNext, firstMove, handleClick, board, winner]);
 
   const renderSquare = (index: any) => (
     // Render a square button
@@ -61,7 +73,19 @@ export default function Home() {
 
   return (
     <>
+      <span className="flex flex-row text-2xl font-bold text-white select-none">
+        <h1 className="mr-2">
+          {score.X}-{score.O}
+        </h1>
+        <button
+          className="px-2 transition-transform duration-700 transform scale-75 hover:rotate-90"
+          onClick={handleResetScore} // Call handleResetScore when the button is clicked
+        >
+          <FiRefreshCw />
+        </button>
+      </span>
       <span className="mb-5 text-2xl font-bold text-white select-none">
+        {/*Display the game status*/}
         {status}
       </span>
       <section className="grid grid-cols-3 gap-3 text-white select-none">
@@ -72,7 +96,7 @@ export default function Home() {
         {(winner || isDraw) && (
           // Show the "Play Again" button if there's a winner or it's a draw
           <button
-            className="py-2 px-4 text-white rounded select-none bg-zinc-800 absolute hover:bg-zinc-900 transition duration-300"
+            className="absolute py-2 px-4 text-white rounded transition duration-300 select-none bg-zinc-800 hover:bg-zinc-900"
             onClick={handleRestart} // Call handleRestart when the button is clicked
           >
             Play Again
